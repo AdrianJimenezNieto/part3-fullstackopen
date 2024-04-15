15dd6a75666384cec6f3bf1b48e88a7e7f841836
adrianJ <123407879+AdrianJimenezNieto@users.noreply.github.com>
2024-04-15 12:33:55 +0200

    exercise 3.5

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
+    id: 1,
+    name: "Arto Hellas", 
+    number: "040-123456"
   },
   { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
+    id: 2,
+    name: "Ada Lovelace", 
+    number: "39-44-5323523"
   },
   { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
+    id: 3,
+    name: "Dan Abramov", 
+    number: "12-43-234345"
   },
   { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
+    id: 4,
+    name: "Mary Poppendieck", 
+    number: "39-23-6423122"
   }
 ]
+
+// GENERATE ID
+const generateId = () =>{
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => n.id))
+    : 0
+  return maxId + 1
+}
 
 // GET ALL ENTRIES
 app.get('/api/persons', (request, response) => {
@@ -62,6 +72,32 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter(note => note.id !== id)
 
   response.status(204).end()
+})
+
+// CREATE AN ENTRY
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name missing'
+    })
+  }
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'number missing'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
 })
 
 // RUNNING THE APP
