9c0777b410af84d939c4ba0dabb3d4d61d5c4df1
adrianJ <123407879+AdrianJimenezNieto@users.noreply.github.com>
2024-04-18 12:47:12 +0200

    exercise 3.15

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,15 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
+
 const app = express()
 
 app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 
-let persons = [
-  { 
-    id: 1,
-    name: "Arto Hellas", 
-    number: "040-123456"
-  },
-  { 
-    id: 2,
-    name: "Ada Lovelace", 
-    number: "39-44-5323523"
-  },
-  { 
-    id: 3,
-    name: "Dan Abramov", 
-    number: "12-43-234345"
-  },
-  { 
-    id: 4,
-    name: "Mary Poppendieck", 
-    number: "39-23-6423122"
-  }
-]
 
 morgan.token('body', (request, response) => {
   if (request.method === 'POST') {
@@ -38,24 +19,17 @@ morgan.token('body', (request, response) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-// GENERATE ID
-const generateId = () =>{
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => n.id))
-    : 0
-  return maxId + 1
-}
-
-// CHECK NAMES
-const checkName = (name) => {
-  return persons.map(person => person.name).includes(name)
-}
-
-// GET ALL ENTRIES
+// GET ALL PEOPLE
 app.get('/api/persons', (request, response) => {
-  response.status(200).json(persons)
+  Person
+    .find({})
+    .then(persons => {
+      response.json(persons)
+    })
 })
 
+
+// ROUTES
 // INFO ROUTE
 app.get('/info', (request, response) => {
   const entries = persons.length
@@ -71,24 +45,26 @@ app.get('/info', (request, response) => {
 
 // GET JUST AN ENTRY
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).json({
-      error: 'Person not found'
+  Person
+    .findById(request.params.id)
+    .then(person => {
+      response.json(person)
     })
-  }
+    .catch(error => {
+      response.status(404).json({
+        error: 'person not found'
+      })
+    })
 })
 
 // DELETE AN ENTRY
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+  Person
+    .findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 // CREATE AN ENTRY
@@ -96,30 +72,27 @@ app.post('/api/persons', (request, response) => {
   const body = request.body
 
   if (!body.name) {
-    return response.status(400).json({
+    return response.status(400).send({
       error: 'name missing'
     })
   }
   else if (!body.number) {
-    return response.status(400).json({
+    return response.status(400).send({
       error: 'number missing'
     })
   }
-  else if (checkName(body.name)) {
-    return response.status(400).json({
-      error: 'name must be unique'
-    })
-  }
 
-  const person = {
+  const person = new Person({
     name: body.name,
-    number: body.number,
-    id: generateId()
-  }
+    number: body.number
+  })
 
-  persons = persons.concat(person)
-
-  response.json(person)
+  person
+    .save()
+    .then(newPerson => {
+      response.json(newPerson)
+    })
+    .catch(error => next(error))
 })
 
 // RUNNING THE APP
